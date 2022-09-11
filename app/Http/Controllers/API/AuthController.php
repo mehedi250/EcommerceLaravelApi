540b1279c0d6aa690bdae370bde57611b8c275af
mehedihasan250 <mehedi@orangetoolz.com>
2022-09-11 22:55:53 +0600

    feat: add env variable

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
