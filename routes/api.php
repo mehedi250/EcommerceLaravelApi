76e0401b310bf6774be103c739ea780fc3df2213
mehedihasan250 <mehedi@orangetoolz.com>
2022-10-22 18:57:06 +0600

    feat: add role and moddleware

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,16 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->group( function(){
+Route::middleware(['auth:sanctum', 'isAPIAdmin'])->group( function(){
     Route::get('checkAuthenticated', function(){
         return response()->json(['message' => 'You are in', 'status'=>200], 200);
     });
-    Route::post('logout', [AuthController::class, 'logout']);
+   
 });
 
+Route::middleware(['auth:sanctum'])->group( function(){
+    Route::post('logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
