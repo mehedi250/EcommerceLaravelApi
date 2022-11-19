9cb59a24435fcebe108114091f7ad7fcd95f22fc
mehedihasan250 <mehedi@orangetoolz.com>
2022-11-19 18:50:06 +0600

    feat: product controller and repo added

---
+++ app/Interfaces/Repository/ProductRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Interfaces\Repository;
+use Illuminate\Http\Request;
+
+interface ProductRepository
+{
+    public function getAll();
+
+    public function getById($id);
+
+    public function insert($data);
+
+    public function update($id, $data);
+
+    public function delete($id);
+}
